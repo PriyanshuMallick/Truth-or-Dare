35bdcf1d2ed443b8322411419bf22553afa45ff5
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-03-22 02:16:08 +0530

    Font changed ✨🪄

--- lib/theme/app_styles.dart
+++ lib/theme/app_styles.dart
@@ -9,14 +9,32 @@ class AppStyles {
     fontWeight: FontWeight.w900,
   );
   static const TextStyle headLineStyle1 = TextStyle(
-    fontFamily: "Pattaya",
+    fontFamily: 'Pattaya',
     fontSize: 91,
     color: AppColors.textColor1,
     fontWeight: FontWeight.w400,
   );
-  static const TextStyle headLineStyle3 = TextStyle(
-    fontFamily: "Pattaya",
+  static TextStyle headLineStyle2 = GoogleFonts.pirataOne(
+    // fontFamily: 'Pattaya',
+    fontSize: 80,
+    color: AppColors.textColor1,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle headLineStyle3 = GoogleFonts.pirataOne(
+    // fontFamily: 'pirataOne',
+    fontSize: 42.45,
+    color: AppColors.textColor1,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle headLineStyle4 = GoogleFonts.pirataOne(
+    // fontFamily: 'pirataOne',
     fontSize: 26,
+    color: AppColors.textColor1,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle headLineStyle5 = GoogleFonts.pirataOne(
+    // fontFamily: 'pirataOne',
+    fontSize: 23,
     color: AppColors.textColor1,
     fontWeight: FontWeight.w400,
   );
