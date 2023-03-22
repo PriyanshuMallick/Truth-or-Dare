88bf40f7ee11aac45b5639c9a67d828c390c84b9
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-03-22 20:16:08 +0530

    + Rotated Rotated Pattern Widget 🦙🦙

--- lib/screens/question_screen_2.dart
+++ lib/screens/question_screen_2.dart
@@ -1,12 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:truthordare/theme/app_gradients.dart';
-import 'package:truthordare/theme/app_styles.dart';
-import 'package:truthordare/widgets/fat_buttons_2.dart';
-import 'package:truthordare/widgets/Game_card.dart';
+import 'package:truthordare/widgets/repeating_bg.dart';
 
+import '../module/questions.dart';
 import '../theme/app_colors.dart';
-import '../utils/app_layout.dart';
+import '../theme/app_consts.dart';
+import '../theme/app_gradients.dart';
+import '../theme/app_styles.dart';
+import '../widgets/fat_buttons_2.dart';
+import '../widgets/game_card.dart';
 
 class QuestionScreen extends StatelessWidget {
   final bool isTruth;
@@ -14,55 +18,75 @@ class QuestionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Random rand = Random();
+    int index = rand.nextInt(Questions.length);
     return Scaffold(
       backgroundColor: isTruth ? AppColors.truthBG : AppColors.dareBG,
-      body: Container(
-        height: AppLayout.getScreenHeight(),
-        width: AppLayout.getScreenWidth(),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.center,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.transparent,
-              Color.fromARGB(50, 0, 0, 0),
-            ],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              isTruth ? 'TRUTH' : 'DARE',
+      body: Stack(
+        children: [
+          RotatedPattern(
+            column: 5,
+            row: 6,
+            gap: 40,
+            degree: -17,
+            scale: 3.5,
+            widget: Text(
+              isTruth ? '?' : '!',
               style: AppStyles.headLineStyle2.copyWith(
-                color: isTruth ? AppColors.truthText : AppColors.dareText,
+                fontSize: 50,
+                color: const Color.fromARGB(8, 255, 255, 255),
               ),
             ),
-            const Gap(20),
-            GameCard(
-              title: 'Eve',
-              subTitle: 'Sing a song in public',
-              gradient: isTruth ? AppGradients.truthCardBG : AppGradients.dareCardBG,
+          ),
+          Container(
+            height: AppConsts.screenHeight,
+            width: AppConsts.screenWidth,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Color.fromARGB(70, 0, 0, 0),
+                ],
+              ),
             ),
-            const Gap(50),
-            Row(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FatButton(
-                  text: 'COMPLETED',
-                  color: isTruth ? AppColors.truthButton : AppColors.dareButton,
-                  onTap: () {},
+                Text(
+                  isTruth ? 'TRUTH' : 'DARE',
+                  style: AppStyles.headLineStyle2.copyWith(
+                    color: isTruth ? AppColors.truthText : AppColors.dareText,
+                  ),
                 ),
-                const Gap(15),
-                FatButton(
-                  text: 'FORFEIT',
-                  color: isTruth ? AppColors.truthButton : AppColors.dareButton,
-                  onTap: () {},
+                const Gap(20),
+                GameCard(
+                  title: 'Eve',
+                  subTitle: isTruth ? Questions.truth[index] : Questions.dare[index],
+                  gradient: isTruth ? AppGradients.truthCardBG : AppGradients.dareCardBG,
                 ),
+                const Gap(50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FatButton(
+                      text: 'COMPLETED',
+                      color: isTruth ? AppColors.truthButton : AppColors.dareButton,
+                      onTap: () {},
+                    ),
+                    const Gap(15),
+                    FatButton(
+                      text: 'FORFEIT',
+                      color: isTruth ? AppColors.truthButton : AppColors.dareButton,
+                      onTap: () {},
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/repeating_bg.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../utils/app_layout.dart';
+
+class RotatedPattern extends StatelessWidget {
+  final int column;
+  final int row;
+  final double gap;
+  final double degree;
+  final double scale;
+  final Widget widget;
+
+  const RotatedPattern({
+    super.key,
+    required this.column,
+    required this.row,
+    required this.gap,
+    required this.degree,
+    required this.scale,
+    required this.widget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Transform.rotate(
+        angle: degree * pi / 180,
+        child: Transform.scale(
+          scale: scale,
+          child: buildPattern(widget),
+        ),
+      ),
+    );
+  }
+
+  Column buildPattern(Widget widget) {
+    List<Widget> rows = [];
+    List<Widget> cols = [];
+    int x = 0;
+    for (int i = 0; i < column; i++) {
+      rows.clear();
+      x = row + (i % 2);
+      for (int j = 0; j < x; j++) {
+        rows.add(widget);
+        if (j != x - 1) {
+          rows.add(Gap(AppLayout.getWidth(gap)));
+        }
+      }
+      cols.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: rows.toList(),
+      ));
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: cols,
+    );
+  }
+}
