d3e7e49c55d54792b17dd1444443501ee6f97e18
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-03-23 00:50:58 +0530

    + Animated Stacked Cards

--- lib/widgets/Game_card.dart
+++ lib/widgets/Game_card.dart
@@ -21,7 +21,7 @@ class GameCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _StackedCards(
+    return _AniStackedCards(
       repeat: 3,
       gradient: gradient,
       child: Column(
@@ -119,5 +119,119 @@ class _StackedCards extends StatelessWidget {
     }
 
     return cards;
+  }
+}
+
+class _AniStackedCards extends StatefulWidget {
+  final int repeat;
+  final Widget child;
+  final Color? color;
+  final List<Gradient> gradient;
+
+  final double scaleFactor;
+  final double opacityFactor;
+
+  const _AniStackedCards({
+    required this.child,
+    required this.gradient,
+    this.repeat = 1,
+    this.scaleFactor = .11,
+    this.opacityFactor = .2,
+    this.color,
+  });
+
+  @override
+  State<_AniStackedCards> createState() => _AniStackedCardsState();
+}
+
+class _AniStackedCardsState extends State<_AniStackedCards> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+
+  int currentIndex = 0;
+  bool isAnimating = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(-1, 0),
+    ).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (!isAnimating) {
+      setState(() {});
+      _animationController.value = details.delta.dx / context.size!.width;
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_animationController.value > 0.5) {
+      isAnimating = true;
+      _animationController.forward().then((_) {
+        setState(() {
+          currentIndex++;
+          isAnimating = false;
+        });
+      });
+    } else {
+      isAnimating = true;
+      _animationController.reverse().then((_) {
+        setState(() {
+          isAnimating = false;
+        });
+      });
+    }
+  }
+
+  Widget _buildCardWidget(int index) {
+    final double gap = 445 / (widget.scaleFactor * 100) - 30;
+    final double topPadding = gap * index;
+
+    return Padding(
+      padding: EdgeInsets.only(top: topPadding),
+      child: Transform.scale(
+        scale: 1 - widget.scaleFactor * (widget.repeat - index - 1),
+        alignment: Alignment.topCenter,
+        child: SlideTransition(
+          position: _slideAnimation,
+          child: _Card(
+            gradient: widget.gradient[index < widget.gradient.length ? index : widget.gradient.length - 1],
+            color: index != widget.repeat - 1
+                ? Colors.black.withOpacity(widget.opacityFactor * (widget.repeat - index - 1))
+                : null,
+            child: index == widget.repeat - 1 ? widget.child : null,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
+      child: Stack(
+        children: [
+          for (int i = currentIndex; i < currentIndex + widget.repeat; i++)
+            if (i >= 0) _buildCardWidget(i),
+        ],
+      ),
+    );
   }
 }
