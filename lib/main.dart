54790e93f89f2779810cdf26896f7aa1f677c9af
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-03-24 10:02:36 +0530

    Misc. ✨✨

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'module/questions.dart';
-import 'screens/game_screen.dart';
 import 'module/question_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GameScreen(),
+      home: HomeScreen(),
     );
   }
 }
