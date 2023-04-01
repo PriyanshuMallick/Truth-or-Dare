99924c8e6602446b5c7e1a36de147c2d16c80582
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-02 00:39:56 +0530

    + new module ⚡🧩

---
+++ lib/module/screen_navs.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScreenNavs {
+  final String title;
+  final IconData icon;
+  final Widget screen;
+
+  ScreenNavs({
+    required this.title,
+    required this.icon,
+    required this.screen,
+  });
+}
