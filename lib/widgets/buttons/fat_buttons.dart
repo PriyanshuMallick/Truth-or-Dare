c7e09589286580e03a18c61fbcc4e9f0858f39db
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-02 00:42:39 +0530

    + customization

--- lib/widgets/buttons/fat_buttons.dart
+++ lib/widgets/buttons/fat_buttons.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
-import '../utils/app_layout.dart';
-import '../theme/app_styles.dart';
+import '../../theme/app_styles.dart';
+import '../../utils/app_layout.dart';
 
 class FatButton extends StatelessWidget {
   final String text;
   final Color bgColor;
   final Function onTap;
+  final double height;
+  final double width;
   const FatButton({
     super.key,
     required this.text,
     required this.bgColor,
     required this.onTap,
+    this.height = 51,
+    this.width = 135,
   });
 
   @override
@@ -19,8 +23,8 @@ class FatButton extends StatelessWidget {
     return GestureDetector(
       onTap: () => onTap(),
       child: Container(
-        height: AppLayout.getHeight(51),
-        width: AppLayout.getWidth(135),
+        height: AppLayout.getHeight(height),
+        width: AppLayout.getWidth(width),
         decoration: BoxDecoration(
           color: bgColor,
           borderRadius: BorderRadius.circular(5),
