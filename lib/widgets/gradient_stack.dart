6e6e2c4e8b16583cad04f109e55241a8bf2f8120
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-01 21:02:58 +0530

    Minor Fix ⚙️

--- lib/widgets/gradient_stack.dart
+++ lib/widgets/gradient_stack.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 class GradientStack extends StatelessWidget {
   final List<Gradient> gradients;
   final Widget child;
-  GradientStack({super.key, required this.gradients, required this.child});
-  int index = -1;
+  const GradientStack({
+    super.key,
+    required this.gradients,
+    required this.child,
+  });
   @override
   Widget build(BuildContext context) {
-    return gradients.isNotEmpty ? getGradients() : child;
+    return gradients.isNotEmpty ? getGradients(0) : child;
   }
 
-  Widget getGradients() {
-    index++;
+  Widget getGradients(int index) {
     return Container(
       decoration: BoxDecoration(gradient: gradients[index]),
-      child: gradients.length - index > 1 ? getGradients() : child,
+      child: gradients.length - index > 1 ? getGradients(++index) : child,
     );
   }
 }
