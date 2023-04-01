6ca181342b7102b261be31285b4a2e8256f6d88f
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-02 00:53:59 +0530

    Update home screen⚡🪄

--- lib/screens/pre_game/home_screen.dart
+++ lib/screens/pre_game/home_screen.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:truthordare/module/lists/bottom_navs_list.dart';
-import 'package:truthordare/theme/app_colors.dart';
-import 'package:truthordare/theme/app_consts.dart';
-import 'package:truthordare/theme/app_gradients.dart';
-import 'package:truthordare/theme/app_styles.dart';
-import 'package:truthordare/utils/app_layout.dart';
-import 'package:truthordare/widgets/fat_buttons.dart';
-import 'package:truthordare/widgets/gradient_stack.dart';
 
-import '../module/question_provider.dart';
-import 'question_screen.dart';
+import '../../theme/app_colors.dart';
+import '../../theme/app_gradients.dart';
+import '../../theme/app_styles.dart';
+import '../../widgets/buttons/fat_buttons.dart';
+import '../../widgets/stylish/gradient_stack.dart';
+import '../../utils/question_provider.dart';
+import '../question_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -23,7 +20,6 @@ class HomeScreen extends StatelessWidget {
         AppGradients.transparent2Black,
       ],
       child: Scaffold(
-        bottomNavigationBar: getBottomNavBar(),
         backgroundColor: Colors.transparent,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -72,50 +68,5 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  Widget getBottomNavBar() {
-    return GradientStack(
-        gradients: AppGradients.bottomNavBG,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              height: 1,
-              width: AppConsts.screenWidth,
-              color: AppColors.homeScreenBottomNavBorder,
-            ),
-            Row(
-              children: getBottomNavs(
-                bottomNavsList,
-                width: AppConsts.screenWidth / bottomNavsList.length,
-                height: AppLayout.getHeight(60),
-                iconSize: 22,
-              ),
-            ),
-          ],
-        ));
-  }
-
-  List<Widget> getBottomNavs(List navList, {required double width, required double height, required double iconSize}) {
-    List<Widget> list = [];
-    for (var i = 0; i < navList.length; i++) {
-      list.add(
-        GestureDetector(
-          onTap: () => navList[i]['onTap'](),
-          child: SizedBox(
-            // color: Colors.transparent,
-            height: height,
-            width: width,
-            child: Icon(
-              navList[i]['icon'],
-              color: AppColors.homeScreenIcon,
-              size: iconSize,
-            ),
-          ),
-        ),
-      );
-    }
-    return list;
   }
 }
