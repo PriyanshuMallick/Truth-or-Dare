5fc0625e5e97cb8c025138f2b2316a6f5caa8261
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-02 13:54:23 +0530

    Fix: No Empty String will be generated

--- lib/utils/questions.dart
+++ lib/utils/questions.dart
@@ -4,35 +4,44 @@ import 'package:csv/csv.dart';
 import 'package:flutter/services.dart';
 
 void makeQuestions() {
-  readCSV();
+  Questions.readCSV();
 }
 
 class Questions {
   static Random rand = Random();
-  static int length = 0;
-  static List<String> truth = [];
-  static List<String> dare = [];
+  static final List<String> _truth = [];
+  static final List<String> _dare = [];
 
-  static String get randomTruth => truth[rand.nextInt(truth.length)];
-  static String get randomDare => dare[rand.nextInt(dare.length)];
-}
+  static int get dareLength => _dare.length;
+  static int get truthLength => _truth.length;
 
-Future<void> readCSV() async {
-  String csvString;
-  try {
-    // This CSV file in not on GitHub
-    csvString = await rootBundle.loadString('assets/___/Truth-or-Dare-Questions - Sheet1.csv');
-  } catch (e) {
-    csvString = await rootBundle.loadString('assets/questions.csv');
+  static String get randomTruth => getRandomText(_truth, _truth.length);
+  static String get randomDare => getRandomText(_dare, _dare.length);
+
+  static String getRandomText(List<String> list, int max, {int min = 0}) {
+    String text = list[rand.nextInt(_dare.length)];
+    while (text.isEmpty) {
+      text = list[rand.nextInt(_dare.length)];
+    }
+    return text;
   }
-  final List<List<dynamic>> rows = const CsvToListConverter().convert(csvString);
+
+  static Future<void> readCSV() async {
+    String csvString;
+    try {
+      // This CSV file in not on GitHub
+      csvString = await rootBundle.loadString('assets/___/Truth-or-Dare-Questions - Sheet1.csv');
+    } catch (e) {
+      csvString = await rootBundle.loadString('assets/questions.csv');
+    }
+    final List<List<dynamic>> rows = const CsvToListConverter().convert(csvString);
 
 // Remove the header row from the list of rows
-  rows.removeAt(0);
+    rows.removeAt(0);
 
-  for (var row in rows) {
-    Questions.truth.add(row[0].toString());
-    Questions.dare.add(row[1].toString());
-    Questions.length++;
+    for (var row in rows) {
+      _truth.add(row[0].toString());
+      _dare.add(row[1].toString());
+    }
   }
 }
