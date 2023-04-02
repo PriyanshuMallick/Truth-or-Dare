f1615bca04e45b0ff3c59f7fce44a00a411442a2
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-02 14:28:41 +0530

    + Game Rules👾👾

---
+++ lib/settings/game_rules.dart
@@ -0,0 +1,9 @@
+class GameRules {
+  static int darePenalty = 1;
+  static int truthPenalty = 1;
+
+  static int dareScore = 2;
+  static int truthScore = 2;
+
+  static int totalForfietable = 0; // 0 = Infinate
+}
