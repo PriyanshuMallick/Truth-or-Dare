0aefdf41a8b1748aa4ca807dbb39466ae06f9a04
PriyanshuMallick <76875358+PriyanshuMallick@users.noreply.github.com>
2023-04-12 23:35:50 +0530

    Implemention Changes: Major ⛏️🫱🏾‍🫲🏾

--- lib/screens/bottom_nav_bar.dart
+++ lib/screens/bottom_nav_bar.dart
@@ -1,33 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:truthordare/providers/screen_provider.dart';
 import 'package:truthordare/utils/lists/bottom_navs_list.dart';
 import 'package:truthordare/widgets/nav_bar.dart';
 
-class BottomNavBar extends StatefulWidget {
+class BottomNavBar extends StatelessWidget {
   const BottomNavBar({super.key});
-
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
-
-class _BottomNavBarState extends State<BottomNavBar> {
-  // ignore: unused_field
-  int _selectedScreenIndex = 1;
-
-  void _onTap(int index) {
-    setState(() {
-      _selectedScreenIndex = index;
-    });
-  }
+  final _sProvider = Provider.of<ScreenProvider>;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      //   body: getTestScreen('Test'),
-      body: bottomNavsList[_selectedScreenIndex].screen,
+      body: bottomNavsList[_sProvider(context).selectedScreenIndex].screen,
       bottomNavigationBar: NavBar(
         navList: bottomNavsList,
-        onTap: (index) => _onTap(index),
+        onTap: (index) => _sProvider(context, listen: false).changeBottomNavScreen(index),
       ),
     );
   }
